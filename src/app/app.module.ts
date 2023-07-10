55520169196e14ba1c4b72d027f707b1f19fc354
Gajava <nikithagajavalli@gmail.com>
2023-07-10 10:56:19 +0530

    frist commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TitleCasePipe } from './title-case.pipe';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitleCasePipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
